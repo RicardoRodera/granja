e541769d326a14244a1ba8b59d867e177e7844cf
Sergio <sergiocb14@educastur.es>
2021-11-24 12:48:07 +0100

    commit2

--- src/Granja.java
+++ src/Granja.java
@@ -18,7 +18,7 @@ public class Granja
     
     public String toString(){
         StringBuilder sb = new StringBuilder();
-        //mecagoentuputamadre
+        //yano pobrecita
         sb.append("\nGrangasxzxzx");
         sb.append("\n====================");
         sb.append("\nNombre: ");
