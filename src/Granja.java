5639ce9870b9d899301dc589ad1d6cc6d57c30bb
Cristian <cristianfv61@educastur.es>
2021-11-24 12:49:40 +0100

    he cambiado la misma linea

--- src/Granja.java
+++ src/Granja.java
@@ -18,7 +18,7 @@ public class Granja
     
     public String toString(){
         StringBuilder sb = new StringBuilder();
-        //yano pobrecita
+        //para ya Cesar 
         sb.append("\nGrangasxzxzx");
         sb.append("\n====================");
         sb.append("\nNombre: ");
